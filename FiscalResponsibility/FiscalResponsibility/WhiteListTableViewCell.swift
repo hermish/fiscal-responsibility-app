2f2f7b2baf37a33e98fdca7d203b6078557d6d7e
Michael Mendioola <mendiola.67@osu.edu>
2018-01-11 16:00:06 -0500

    Main page initial commit

---
+++ FiscalResponsibility/FiscalResponsibility/WhiteListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  WhiteListTableViewCell.swift
+//  FiscalResponsibility
+//
+//  Created by Michael Mendiola on 1/11/18.
+//  Copyright © 2018 CapitalOneSummit. All rights reserved.
+//
+
+import UIKit
+
+class WhiteListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var whiteListLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
