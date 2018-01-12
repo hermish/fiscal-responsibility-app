0dc8afc3ac1da200c78341877a6de83aef15e2c1
Michael Mendioola <mendiola.67@osu.edu>
2018-01-11 20:00:44 -0500

    bc i have to

--- FiscalResponsibility/FiscalResponsibility/AddUserViewController.swift
+++ FiscalResponsibility/FiscalResponsibility/AddUserViewController.swift
@@ -22,14 +22,13 @@ class AddUserViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
-        
         self.dismiss(animated: true, completion: nil)
     }
     
