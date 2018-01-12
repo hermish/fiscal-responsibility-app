93f2c35df70b8bf6812d723923379e19b1b453c9
David Liu <xiaomingshuo@gmail.com>
2018-01-12 03:07:26 -0500

    category page added

---
+++ FiscalResponsibility/FiscalResponsibility/CategoryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryTableViewCell.swift
+//  FiscalResponsibility
+//
+//  Created by David Liu on 1/12/18.
+//  Copyright © 2018 CapitalOneSummit. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
