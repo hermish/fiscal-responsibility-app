0d6c1accf897539e1324224a504644b95fbb1d0e
Jonson Jin <jonsonj@umich.edu>
2018-01-11 22:49:28 -0500

    Added map stuff

--- FiscalResponsibility/FiscalResponsibility/MapAddViewController.swift
+++ FiscalResponsibility/FiscalResponsibility/MapAddViewController.swift
@@ -13,11 +13,13 @@ class MapAddViewController: UIViewController, UISearchBarDelegate {
 	
 	@IBOutlet weak var mapSearch: UISearchBar!
 	@IBOutlet weak var map: MKMapView!
+	let regionRadius: CLLocationDistance = 1000
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		let startLoc = CLLocation(latitude: 38.886879, longitude: -77.09465183333333)
+		self.centerMapOnLocation(location: startLoc)
 		mapSearch.delegate = self
-		// Do any additional setup after loading the view.
 	}
 	
 	override func didReceiveMemoryWarning() {
@@ -25,7 +27,7 @@ class MapAddViewController: UIViewController, UISearchBarDelegate {
 		// Dispose of any resources that can be recreated.
 	}
 	
-	let regionRadius: CLLocationDistance = 1000
+	
 	func centerMapOnLocation(location: CLLocation) {
 		let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
 																  regionRadius, regionRadius)
